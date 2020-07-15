785c3f9b45ad47b911e86fab97ed1277d280a59a
jebitok-dev <sharonkosgei4@gmail.com>
2020-07-15 06:31:38 +0300

    first

---
+++ landing.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="land.css">
+    <title>SpotSoundOn</title>
+</head>
+<body>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active two">
+              <img src="./images/spoton1.png" class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h2 style="text-transform: uppercase;">Realtime update on SpotSoundOn</h2><hr>
+                <p style="text-transform: capitalize;">Discover, share, stream and get Realtime updates of your favorite music and major artists around the globe</p>
+                <button class="one" style="color: #FFFFFF;">SIGN UP HERE</button>
+            </div>
+          </div>
+          <div class="carousel-item two">
+            <img src="./images/spoton1.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h2 style="text-transform: uppercase;">Realtime update on SpotSoundOn</h2><hr>
+                <p style="text-transform: capitalize;">Discover, share, stream and get Realtime updates of your favorite music and major artists around the globe</p>
+                <button class="one" style="color: #FFFFFF;">SIGN UP HERE</button>
+            </div>
+          </div>
+          <div class="carousel-item two">
+            <img src="./images/spoton1.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h2 style="text-transform: uppercase;">Realtime update on SpotSoundOn</h2>hr>
+                <p style="text-transform: capitalize;">Discover, share, stream and get Realtime updates of your favorite music and major artists around the globe</p>
+                <button class="one" style="color: #FFFFFF;">SIGN UP HERE</button>
+            </div>
+          </div>
+    </div>
+    </div>
+
+<form>
+  <input type="text" placeholder="Search for artists, tracks, bands" name="search" style="width: 700px; height: 70px; background: rgba(196, 196, 196, 0.5);">
+  <button type="submit" style="width: 105px; height: 28px; font-size: 24px; line-height: 29px; color: rgba(255, 255, 255, 0.9);">Search</button>
+</form>
+
+  <div class="row">
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 5.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Control Me</h5>
+        <p class="card-text">Peter Frank</p>
+      </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 6.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Time of My Life</h5>
+        <p class="card-text">Bella Collins</p>
+      </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 7.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Feel Like i'm out of Me</h5>
+        <p class="card-text">Joe Quady</p>
+      </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 8.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">You Choose Me</h5>
+        <p class="card-text">Frank Mark</p>
+      </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 10.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Change the Girl</h5>
+        <p class="card-text">John Porter</p>
+      </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 11.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">You loved me once</h5>
+        <p class="card-text">Stella Mariciol</p>
+      </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 12.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Take me</h5>
+        <p class="card-text">Guardison Frank Juan</p>
+      </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 13.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Call me your friend</h5>
+        <p class="card-text">Jayde Frankle</p>
+      </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 14.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Chi wan Chun</h5>
+        <p class="card-text">Si Mia Ke lar</p>
+      </div>
+    </div>
+    
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="./images/Rectangle 15.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">I hate the game</h5>
+        <p class="card-text">Jenny Blank</p>
+      </div>
+    </div>    
+  </div><hr>
+
+  <button<a href="#" class="btn btn-primary" style="width: 342px; height: 29px; color: #FFFFFF;">Exlore your Latest Playlist</a></button>
+
+
+</body>
+</html>
